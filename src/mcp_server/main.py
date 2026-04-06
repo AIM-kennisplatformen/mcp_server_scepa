de9b771e1b24c560a99400a98b155bd4323e22d4
Simon Dingemans <simon.dingemans@gmail.com>
2026-04-06 03:20:46 +0200

    fixed main

--- src/mcp_server/main.py
+++ src/mcp_server/main.py
@@ -11,9 +11,8 @@ mcp = FastMCP("paper_search")
 mcp.add_tool(get_literature_supported_knowledge)
 
 if __name__ == "__main__":
-    # mcp.run(
-    #     transport="http", 
-    #     host="0.0.0.0",
-    #     port=8001
-    # )
-    print(get_literature_supported_knowledge("What are best practices for energy poverty?", [Keywords.BEST_PRACTICES], [LiteratureType.SCIENTIFIC]))
+    mcp.run(
+        transport="http", 
+        host="0.0.0.0",
+        port=8001
+    )
